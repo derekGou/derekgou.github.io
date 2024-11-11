f90906ea587ea35fc56c07a9ce4b56baae55a1f7
Derek Gou <derekgou19@gmail.com>
2024-11-11 08:39:32 -0500

    intrrobutton fix

--- scripts/eventListeners.js
+++ scripts/eventListeners.js
@@ -8,15 +8,3 @@ window.addEventListener("resize", function(){
     document.getElementById('introButton').style.left = $('#name').offset().left+'px';
     document.getElementById('introButton').style.top = $('#name').offset().top+'px';
 });
-
-window.addEventListener("load", function(){
-    widths = [
-        $('#home').width(),
-        $('#profile').width(),
-        $('#projects').width(),
-        $('#socials').width()
-    ]
-    document.getElementById('introButton').style.left = $('#name').offset().left+'px';
-    document.getElementById('introButton').style.top = $('#name').offset().top+'px';
-    console.log($('#name').offset().top+'px')
-})
